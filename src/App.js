db0adcaedf7887a0931e456834efa11f88e1cb37
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-11-07 16:52:42 +0300

    add home path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Auth from "./Auth"
+import Login from "./Login"
+import Home from "./Home"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
+        <Route path="/login" element={<Login />} />
+        </Routes>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+    
     </BrowserRouter>
   )
 }
