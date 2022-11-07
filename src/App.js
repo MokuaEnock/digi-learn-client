21106785135a214ec099d5766a37649d16901f88
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-11-07 14:13:42 +0300

    adds react routes to login

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./Auth"
+import Login from "./Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
