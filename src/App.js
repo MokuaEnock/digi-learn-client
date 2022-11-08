70822aab9676edd98c7cb7ab28137b229090698c
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-11-08 06:32:34 +0300

    import navbar add routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./Login"
 import Home from "./Home"
+import NavBar from "./Navbar"
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar/>
       <Routes>
         <Route path="/login" element={<Login />} />
         </Routes>
