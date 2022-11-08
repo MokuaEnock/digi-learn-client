0a648e3c0d801461927dd20044536b97bbf8f203
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-11-08 22:58:12 +0300

    adds navigator

--- src/Login.js
+++ src/Login.js
@@ -23,14 +23,7 @@ function Login(props) {
 
   };
   
-  // switch between sign in and sign up
-  //let [authMode, setAuthMode] = useState("signin")
 
-  //const changeAuthMode = () => {
-    //setAuthMode(authMode === "signin" ? "signup" : "signin")
-  //}
-
-  //if (authMode === "signin") {
     return (
       <div className="Auth-form-container">
         <form className="Auth-form" onSubmit={handleSubmit}>
@@ -78,57 +71,6 @@ function Login(props) {
         </form>
       </div>
     )
-  //}
 
-  // return (
-  //   <div className="Auth-form-container">
-  //     <form className="Auth-form" onSubmit={handleSubmit}>
-  //       <div className="Auth-form-content">
-  //         <h3 className="Auth-form-title">Sign Up</h3>
-  //         <div className="text-center">
-  //           Already registered?{" "}
-  //           <span className="link-primary" onClick={changeAuthMode}>
-  //             Sign In
-  //           </span>
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Full Name</label>
-  //           <input
-  //             type="text"
-  //             className="form-control mt-1"
-  //             placeholder="e.g Jane Doe"
-  //             required
-  //           />
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Email address</label>
-  //           <input 
-  //             type="email"
-  //             className="form-control mt-1"
-  //             placeholder="Email Address"
-  //             required
-  //           />
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Password</label>
-  //           <input
-  //             type="password"
-  //             className="form-control mt-1"
-  //             placeholder="Password"
-  //             required
-  //           />
-  //         </div>
-  //         <div className="d-grid gap-2 mt-3">
-  //           <button type="submit" className="btn btn-primary">
-  //             Sign Up
-  //           </button>
-  //         </div>
-  //         <p className="text-center mt-2">
-  //           Forgot <a href="#">password?</a>
-  //         </p>
-  //       </div>
-  //     </form>
-  //   </div>
-  // )
 }
 export default Login
