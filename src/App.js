57fe68a7a3c6c2239086e59e97be12e8a798d036
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-11-09 15:04:31 +0300

    Add contact component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./Login"
 import Signup from "./Signup"
 import Forgot from "./Forgot"
+import Contact from "./Contact"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/resetpassword" element={<Forgot />} />
+        <Route path="/contactus" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
