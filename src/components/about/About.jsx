4f013caa715a3164dd1adc3a98d988ad306562c2
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-09 23:49:54 +0300

    forat code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,27 +5,37 @@ import "./about.css";
 import Heading from "../header/Heading";
 import Awrapper from "..App//Awrapper";
 
-function About(){
-    return(
-        <>
-        <section id="about" className='about'>
-          {/* <Back name='' title='About Us - Who We Are?'  /> */}
-          <div className='container flex mtop'>
-            <div className='left row'>
-              <Heading title='Our  Story' subtitle='Check out our company story ' />
-  
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit necessitatibus numquam eos reiciendis alias, expedita, quidem explicabo hic voluptates voluptate dolor repudiandae quis corrupti nulla, iste mollitia similique dolorum animi?</p>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi praesentium vero repellendus animi commodi architecto qui, ipsam reiciendis odio eligendi ut? Reprehenderit, aut itaque impedit accusantium alias quaerat culpa distinctio.</p>
-              {/* <button className='btn2'>More About Us</button> */}
-            </div>
-            <div className='right row'>
-            
-            </div>
+function About() {
+  return (
+    <>
+      <section id="about" className="about">
+        {/* <Back name='' title='About Us - Who We Are?'  /> */}
+        <div className="container flex mtop">
+          <div className="left row">
+            <Heading
+              title="Our  Story"
+              subtitle="Check out our company story "
+            />
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              necessitatibus numquam eos reiciendis alias, expedita, quidem
+              explicabo hic voluptates voluptate dolor repudiandae quis corrupti
+              nulla, iste mollitia similique dolorum animi?
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Excepturi praesentium vero repellendus animi commodi architecto
+              qui, ipsam reiciendis odio eligendi ut? Reprehenderit, aut itaque
+              impedit accusantium alias quaerat culpa distinctio.
+            </p>
+            {/* <button className='btn2'>More About Us</button> */}
           </div>
-        </section>
-        <Awrapper/>
-      </>
-    )
+          <div className="right row"></div>
+        </div>
+      </section>
+      <Awrapper />
+    </>
+  );
 }
 export default About;
-
--- src/components/about/Awrapper.jsx
+++ src/components/about/Awrapper.jsx
@@ -1,31 +1,28 @@
-import React from "react"
-import { awrapper } from "../../components/Data"
+import React from "react";
+import { awrapper } from "../../components/Data";
 
 const Awrapper = () => {
   return (
     <>
-      <section className='awrapper'>
-        <div className='container grid'>
+      <section className="awrapper">
+        <div className="container grid">
           {awrapper.map((val) => {
             return (
-              <div className='box flex'>
-                <div className='img'>
-                  <img src={val.cover} alt='' />
+              <div className="box flex">
+                <div className="img">
+                  <img src={val.cover} alt="" />
                 </div>
-                <div className='text'>
+                <div className="text">
                   <h1>{val.data}</h1>
                   <h3>{val.title}</h3>
                 </div>
               </div>
-            )
+            );
           })}
-          
-        
         </div>
-        
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Awrapper
+export default Awrapper;
