a0729379a764c7b620cf14071948a067a45d33a6
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-11-09 22:13:00 +0300

    add new column

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,37 @@ import {image,pic} from "./Data"
 function Home(){
     return(
         <>
-    <div className='homeparent'>
-        <h1>Putting Your Own Future<br></br>
-        in Great Motion</h1>
-       <div className="page" >
-       <img src={image.url} alt="congrats"/>
-                <div className="centre">
-                    <ul>
-                        <li><span style={{background:"red"}}>&#x2611;</span>Free</li>
-                        <li><span style={{color:"red"}}>&#x2611;</span>Fast</li>
-                        <li><span style={{color:"red"}}>&#x2611;</span>Beginner Friendly</li>
-                    </ul>
-                    <button >Get Started Now</button>
-                    <p><span style={{color:"red",fontSize:"bold"}}>&#8594;</span>Proving you with a course management<br></br>
-                    system to track your lectures and students</p>
+                <div className='homeparent'>
+                    <h1>Putting Your Own Future<br></br>
+                    in Great Motion</h1>
+                        <div className="page" >
+                            <  img src={image.url} alt="congrats"/>
+                                        <div className="centre">
+                                            <ul>
+                                                <li><span style={{color:"red"}}>&#x2611;</span>Free</li>
+                                                <li><span style={{color:"red"}}>&#x2611;</span>Fast</li>
+                                                <li><span style={{color:"red"}}>&#x2611;</span>Beginner Friendly</li>
+                                            </ul>
+                                            <button style={{background:"#FF033E"}}>Get Started Now</button>
+                                            <p><span style={{color:"red",}}>&#8594;</span>Proving you with a course management<br></br>
+                                            system to track your lectures and students</p>
+                                        </div>
+                                <img src={pic.url} alt="congrats"/>
+                        </div>
                 </div>
-       <img src={pic.url} alt="congrats"/>
-       </div>
-    </div>
+            <div className="column">
+                <div id="column_li">05 +
+                  <p>Free Courses</p>
+                </div>
+                <div id="column_li">07 +
+                    <p>Qualified Lectures</p>
+                </div>
+                <div id="column_li">100 +
+                    <p>Ready Students</p>
+                </div>
+
+            </div>
+   
 
     </>)
 
