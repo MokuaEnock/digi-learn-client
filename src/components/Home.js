c60097b07730fd0e1eaff204e477beb6ecebc769
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-10 05:49:22 +0300

    update images on landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,72 +1,74 @@
 import React from "react";
-import { image, pic } from "./Data";
+//import { image, pic } from "./Data";
+import Image from "./img/image2.png";
+import Pic from "./img/image3.png";
 
 function Home() {
   return (
     <main>
-    <section className="main_section1">
-      <div className="main_image">
-        <img src="#" alt="student" />
-      </div>
+      <section className="main_section1">
+        <div className="main_image" id="image1">
+          <img src={Image} alt="student" />
+        </div>
 
-      <div className="main_content">
-        <span id="span1">
-          Putting Your Own Future <br />
-          in Great Motion
-        </span>
-        <div id="span2">
-          <span>
-            <img src="#" alt="checkbox" />
-            <p>Free</p>
+        <div className="main_content">
+          <span id="span1">
+            Putting Your Own Future <br />
+            in Great Motion
           </span>
+          <div id="span2">
+            <span>
+              <img src="#" alt="checkbox" />
+              <p>Free</p>
+            </span>
 
-          <span>
-            <img src="#" alt="checkbox" />
-            <p>Fast</p>
-          </span>
+            <span>
+              <img src="#" alt="checkbox" />
+              <p>Fast</p>
+            </span>
 
-          <span>
-            <img src="#" alt="checkbox" />
-            <p>Friendly</p>
+            <span>
+              <img src="#" alt="checkbox" />
+              <p>Friendly</p>
+            </span>
+          </div>
+          <button>Get Started</button>
+          <span id="span3">
+            Providing you with a course management system to track your
+            lecturers and students.
           </span>
         </div>
-        <button>Get Started</button>
-        <span id="span3">
-          Providing you with a course management system to track your
-          lecturers and students.
-        </span>
-      </div>
 
-      <div className="main_image">
-        <img src="#" alt="student" />
-      </div>
-    </section>
-    <section className="main_section2">
-      <div>
-        <span>
-          <span id="color1"></span>
-          <p>05+</p>
-        </span>
-        <span>Free Courses</span>
-      </div>
+        <div className="main_image">
+          <img src={Pic} alt="student" />
+        </div>
+      </section>
+      <section className="main_section2">
+        <div>
+          <span>
+            <span id="color1"></span>
+            <p>05+</p>
+          </span>
+          <span>Free Courses</span>
+        </div>
 
-      <div>
-        <span>
-          <span id="color2"></span>
-          <p>05+</p>
-        </span>
-        <span>Qualified Lecturers</span>
-      </div>
+        <div>
+          <span>
+            <span id="color2"></span>
+            <p>05+</p>
+          </span>
+          <span>Qualified Lecturers</span>
+        </div>
 
-      <div>
-        <span>
-          <span id="color3"></span>
-          <p>05+</p>
-        </span>
-        <span>Ready Students</span>
-      </div>
-    </section>
-  </main>
+        <div>
+          <span>
+            <span id="color3"></span>
+            <p>05+</p>
+          </span>
+          <span>Ready Students</span>
+        </div>
+      </section>
+    </main>
   );
 }
 export default Home;
