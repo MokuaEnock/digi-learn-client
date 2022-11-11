7b4418d2ee08346d81d97495041564bd74dffe31
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-11 05:05:29 +0300

    delete commented code

--- src/components/students/Students.js
+++ src/components/students/Students.js
@@ -2,76 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./student.css";
 
 export default function Students() {
-  // let [student, setStudent] = useState([]);
-  // let [course, setCourse] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:7000/students")
-  //     .then((r) => r.json())
-  //     .then((r) => setStudent(r));
-  // }, []);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:7000/courses")
-  //     .then((r) => r.json())
-  //     .then((r) => setCourse(r));
-  // }, []);
-
-  // function StudentDetailCard() {
-  //   return (
-  //     <aside id="main_student">
-  //       <span id="lecturer_name">Enock Mokua</span>
-  //       <img src="#" alt="lecturer" />
-  //       <span id="lecturer_course">Machine Learning</span>
-  //       <span id="student_number">Students: 117</span>
-  //       <span id="average_grade">Average grade: 78</span>
-  //       <span> Rank: 17</span>
-
-  //       <span id="date_joined">Date Joined: 08/12/2012</span>
-  //     </aside>
-  //   );
-  // }
-
-  // let course_cards = course.map((item) => {
-  //   return (
-  //     <span className="course_item" key={item.id}>
-  //       <span>
-  //         <span>{item.name}</span>
-  //         <span className="pppp">{item.description}</span>
-  //       </span>
-  //       <button>View</button>
-  //     </span>
-  //   );
-  // });
-
-  // function Course() {
-  //   return (
-  //     <div id="courses">
-  //       <span className="title">
-  //         <span>Courses</span>
-  //         <form>
-  //           <button type="submit">Search</button>
-  //           <input type="" required />
-  //         </form>
-  //       </span>
-  //       <div className="course_list">
-  //         {course_cards}
-  //         <button className="view_button">View All</button>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
-  // function Grade() {
-  //   return (
-  //     <>
-  //       <span id="lecturer_course">Machine Learning</span>
-  //       <span id="average_grade">Average grade: 78</span>
-  //       <span> Rank: 17</span>
-  //     </>
-  //   );
-  // }
-
+  
   return (
     <main id="student">
       <section id="profile">
