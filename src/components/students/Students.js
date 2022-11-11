7c021b5564efb64c4397098bdd23ccd2acec5c4d
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-11 05:06:43 +0300

    fetch data

--- src/components/students/Students.js
+++ src/components/students/Students.js
@@ -2,6 +2,46 @@ import React, { useState, useEffect } from "react";
 import "./student.css";
 
 export default function Students() {
+  let [lecturer, setLecturer] = useState([]);
+  let [user, setUser] = useState([]);
+  let [student, setStudent] = useState([]);
+  let [course, setCourse] = useState([]);
+  let [cohort, setCohort] = useState([]);
+  let [searchCourse, setSearchCourse] = useState([]);
+  let [searchStudent, setSearchStudent] = useState([]);
+  let [newStudentItem, setNewStudentItem] = useState([]);
+  let [newCourseItem, setNewCourseItem] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/users")
+      .then((r) => r.json())
+      .then((r) => setUser(r));
+  }, [setUser]);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/lecturers")
+      .then((r) => r.json())
+      .then((r) => setLecturer(r));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/students")
+      .then((r) => r.json())
+      .then((r) => setStudent(r));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/courses")
+      .then((r) => r.json())
+      .then((r) => setCourse(r));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/cohorts")
+      .then((r) => r.json())
+      .then((r) => setCohort(r));
+  }, []);
+
   
   return (
     <main id="student">
